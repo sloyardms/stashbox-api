43307ee78c73eca373f9876d8803bf4cc9ba9ef5
sloyardms <sloyard_ms@outlook.com>
2026-03-15 00:30:48 -0700

    refactor(security): use @AuthenticationPrincipal instead of AuthenticatedUserProvider

--- src/main/java/com/sloyardms/stashboxapi/security/controller/TestSecurityController.java
+++ src/main/java/com/sloyardms/stashboxapi/security/controller/TestSecurityController.java
@@ -1,10 +1,11 @@
 package com.sloyardms.stashboxapi.security.controller;
 
-import com.sloyardms.stashboxapi.security.AuthenticatedUserProvider;
+import com.sloyardms.stashboxapi.security.AuthenticatedUser;
 import com.sloyardms.stashboxapi.security.annotation.AdminOnly;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +18,16 @@ import java.util.Map;
 @Profile("test")
 public class TestSecurityController {
 
-    private final AuthenticatedUserProvider authenticatedUserProvider;
-
     @GetMapping("/authenticated")
-    public ResponseEntity<Map<String, Boolean>> authenticatedEndpoint() {
-        System.out.println(authenticatedUserProvider.getAuthenticatedUser());
+    public ResponseEntity<Map<String, Boolean>> authenticatedEndpoint(@AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
+        System.out.println(authenticatedUser);
         return ResponseEntity.ok(Map.of("authenticated", true));
     }
 
     @GetMapping("/admin")
     @AdminOnly
-    public ResponseEntity<Map<String, Boolean>> adminEndpoint() {
-        System.out.println(authenticatedUserProvider.getAuthenticatedUser());
+    public ResponseEntity<Map<String, Boolean>> adminEndpoint(@AuthenticationPrincipal AuthenticatedUser authenticatedUser) {
+        System.out.println(authenticatedUser);
         return ResponseEntity.ok(Map.of("admin", true));
     }
 }
