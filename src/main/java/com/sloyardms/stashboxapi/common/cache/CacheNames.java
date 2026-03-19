fb0751f61a4452244d00c3375cc9c2d09e52a639
sloyardms <sloyard_ms@outlook.com>
2026-03-18 17:26:45 -0700

    feat(cache): add CacheNames

---
+++ src/main/java/com/sloyardms/stashboxapi/common/cache/CacheNames.java
@@ -0,0 +1,11 @@
+package com.sloyardms.stashboxapi.common.cache;
+
+public final class CacheNames {
+
+    private CacheNames() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static final String USER_ID_BY_PROVIDER_ID = "userIdByProviderId";
+
+}
