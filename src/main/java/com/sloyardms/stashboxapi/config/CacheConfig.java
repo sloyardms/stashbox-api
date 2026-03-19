9cb7da4b1629f0d6b15c851cf02b6b8a3968ffcc
sloyardms <sloyard_ms@outlook.com>
2026-03-18 17:18:59 -0700

    refactor(cache): change the way the cache is created to facilitate the creation of new caches

--- src/main/java/com/sloyardms/stashboxapi/config/CacheConfig.java
+++ src/main/java/com/sloyardms/stashboxapi/config/CacheConfig.java
@@ -1,25 +1,43 @@
 package com.sloyardms.stashboxapi.config;
 
+import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.sloyardms.stashboxapi.common.cache.CacheNames;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
+    @Value("${cache.record-stats:false}")
+    private boolean recordStats;
+
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("userIdByProviderId");
-        cacheManager.setCaffeine(Caffeine.newBuilder()
-                .expireAfterWrite(24, TimeUnit.HOURS)
-                .maximumSize(50_000)
-        );
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        cacheManager.registerCustomCache(CacheNames.USER_ID_BY_PROVIDER_ID, buildCache(
+                Duration.ofHours(24), 50_000, recordStats
+        ));
+
         return cacheManager;
     }
+
+    private Cache<Object, Object> buildCache(Duration ttl, long maxSize, boolean recordStats) {
+        Caffeine<Object, Object> caffeine = Caffeine.newBuilder()
+                .expireAfterWrite(ttl)
+                .maximumSize(maxSize);
+
+        if (recordStats) caffeine.recordStats();
+
+        return caffeine.build();
+    }
+
 }
