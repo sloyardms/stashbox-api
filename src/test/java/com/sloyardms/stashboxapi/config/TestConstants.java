ea6c625247cd4c6aecf6ae17a9b646aabc3e2f22
sloyardms <sloyard_ms@outlook.com>
2026-04-07 19:37:50 -0700

    feat(test): add TestConstants class for test catalog records IDs

---
+++ src/test/java/com/sloyardms/stashboxapi/config/TestConstants.java
@@ -0,0 +1,17 @@
+package com.sloyardms.stashboxapi.config;
+
+import java.util.UUID;
+
+public class TestConstants {
+
+    public static final UUID USER_ID = UUID.fromString("1a9d5015-5330-46af-959b-d6d9913ad75c");
+    public static final UUID ADMIN_USER_ID = UUID.fromString("438536b5-335b-4182-9f1b-cc3388b3b707");
+
+    // item groups
+    public static final UUID ADMIN_USER_DEFAULT_GROUP_ID = UUID.fromString("a1b2c3d4-0005-4000-8000-000000000005");
+    public static final UUID USER_DEFAULT_GROUP_ID = UUID.fromString("a1b2c3d4-0001-4000-8000-000000000001");
+    public static final UUID USER_GROUP_1_ID = UUID.fromString("a1b2c3d4-0002-4000-8000-000000000002");
+    public static final UUID USER_GROUP_2_ID = UUID.fromString("a1b2c3d4-0002-4000-8000-000000000003");
+    public static final UUID USER_GROUP_3_ID = UUID.fromString("a1b2c3d4-0002-4000-8000-000000000004");
+
+}
