bba43bf02ad5ed01854be4b8ff623a046cb6d197
sloyardms <sloyard_ms@outlook.com>
2026-07-07 16:33:37 -0700

    feat(cache): enable application caching

--- src/main/java/com/sloyardms/stashboxapi/StashboxApiApplication.java
+++ src/main/java/com/sloyardms/stashboxapi/StashboxApiApplication.java
@@ -2,9 +2,11 @@ package com.sloyardms.stashboxapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableCaching
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class StashboxApiApplication {
