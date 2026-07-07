e86a4ecf57ba3f206e6c344757ddde74e34b79e3
sloyardms <sloyard_ms@outlook.com>
2026-07-07 16:24:27 -0700

    feat(dto): add CreateStashItemRequest and StashItemDetailResponse classes

---
+++ src/main/java/com/sloyardms/stashboxapi/domain/stash/dto/response/StashItemDetailResponse.java
@@ -0,0 +1,28 @@
+package com.sloyardms.stashboxapi.domain.stash.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class StashItemDetailResponse {
+
+    private UUID id;
+    private ItemGroupRefResponse group;
+    private String title;
+    private String titleNormalized;
+    private String url;
+    private String urlNormalized;
+    private String description;
+    private String imagePath;
+    private Boolean favorite;
+    private Instant deletedAt;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+}
